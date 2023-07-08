bdcea11229168e4c78479aaeae50a94df8ea589b
Adebowale Otulana <otulana.adebowale@gmail.com>
2023-07-08 09:47:49 -0600

    upgrade cypress and other dependencies

--- cypress/integration/3-cucumber/features/parabank.feature
+++ cypress/integration/3-cucumber/features/parabank.feature
@@ -1,4 +1,9 @@
 Feature: parabank.parasoft.com
   Scenario: visting the login page
-    When I visit parabank.parasoft.com/parabank/index.htm
+    Given I visit parabank.parasoft.com/parabank/index.htm
     Then I should see a login form
+
+  Scenario: login with valid credentials
+    Given I visit parabank.parasoft.com/parabank/index.htm
+    When I provide valid username and password
+    Then I should see user accounts overview
--- cypress/integration/3-cucumber/step-definitions/parabank.ts
+++ cypress/integration/3-cucumber/step-definitions/parabank.ts
@@ -1,9 +1,13 @@
-import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
+import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
-When(/^I visit parabank\.parasoft\.com\/parabank\/index\.htm$/, () => {
+Given(/^I visit parabank\.parasoft\.com\/parabank\/index\.htm$/, () => {
   cy.visit('https://parabank.parasoft.com/parabank/index.htm');
 });
 
 Then('I should see a login form', () => {
   cy.get('#leftPanel > h2').should('have.text', 'Customer Login');
 });
+
+When('I provide valid username and password', () => {});
+
+Then('I should see user accounts overview', () => {});
