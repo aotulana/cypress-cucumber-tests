3b54332aeaba822d0e7b86824d67d3993e8e9ffc
aotulana <44875776+aotulana@users.noreply.github.com>
2023-07-11 01:17:42 +0200

    change bundler to esbuild from browserify (#4)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,19 @@
-import * as browserify from '@cypress/browserify-preprocessor';
 import { defineConfig } from 'cypress';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-import { preprocessor } from '@badeball/cypress-cucumber-preprocessor/browserify';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export async function setupNodeEvents(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): Promise<Cypress.PluginConfigOptions> {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
   await addCucumberPreprocessorPlugin(on, config);
 
   on(
     'file:preprocessor',
-    preprocessor(config, {
-      ...browserify.defaultOptions,
-      typescript: require.resolve('typescript'),
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
     })
   );
 
@@ -27,6 +27,6 @@ export default defineConfig({
     specPattern: '**/*.feature',
     excludeSpecPattern: '*.js',
     setupNodeEvents,
-    supportFile: 'cypress/support/e2e.ts'
+    supportFile: 'cypress/support/e2e.ts',
   },
 });
